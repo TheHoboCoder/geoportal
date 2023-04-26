e17dbb994645e312e01e77f1cc72a37edc02c83c
TheHoboCoder <animator.x@yandex.ru>
2023-04-26 20:04:30 +0300

    changed serializer sample

--- sample_module/serializers.py
+++ sample_module/serializers.py
@@ -2,15 +2,10 @@ from rest_framework import serializers
 from common import serializers as geo_serializers
 
 class DrawLineSerializer(serializers.Serializer):
-    int_param = serializers.IntegerField()
-    string_param = serializers.CharField(max_length=50)
+    int_param = serializers.IntegerField(label="Количество", min_value=1, max_value=5)
+    string_param = serializers.CharField(label="Введите test", max_length=50)
     point_start = geo_serializers.PointField()
     point_end = geo_serializers.PointField()
-
-    def validate_int_param(self, val):
-        if val not in range(0, 5):
-            raise serializers.ValidationError("int param should be in range [0, 5)")
-        return val
 
     def validate_string_param(self, val):
         if "test" not in val:
